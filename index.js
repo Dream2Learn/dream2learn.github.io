277a8c824076b44039d46f50800db95afa0bfe58
​ <>
2021-11-20 23:26:34 -0500

    Add navbar

---
+++ index.js
@@ -0,0 +1,13 @@
+const qs = selector => document.querySelector(selector);
+
+const observer = new IntersectionObserver(([entry], observer) => {
+	if (entry.intersectionRatio > 0.5) {
+		// Header is shown
+	} else {
+		// Header is hidden
+	}
+}, {
+	threshold: 0.5,
+});
+
+observer.observe(qs("header"));
